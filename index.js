441483937f37baacd63f84e75d35d9e7961a69ac
gaowenhan <1049912732@qq.com>
2025-05-22 15:47:12 +0800

    添加List数据接口

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const port = process.env.PORT || 3000
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err))
+
+const dataSchema = new mongoose.Schema({
+  username: String,
+  password: String
+})
+
+const Data = mongoose.model('Data', dataSchema,'Data')
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/List', (req, res) => {
-  res.send({
+app.get('/List', async (req, res) => {
+ const data = await Data.find()
+  res.json({
     code:200,
-    data:{
-      id:1,
-      name:'张三',
-      sex:'男',
-      age:18
-    }
+    message:"success",
+    data
   })
 })
 
