b948dde03ca627f741c5dc18332009490942915a
gaowenhan <1049912732@qq.com>
2025-05-22 15:32:04 +0800

    添加List数据接口

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/List', (req, res) => {
+  res.send({
+    code:200,
+    data:{
+      id:1,
+      name:'张三',
+      sex:'男',
+      age:18
+    }
+  })
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
